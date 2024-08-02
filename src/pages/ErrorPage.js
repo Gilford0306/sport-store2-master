c889c620e0830d16a7ef42399a584444e5efdd57
Gilford0306 <Gilford0306@gmail.com>
2024-08-02 09:30:56 +0300

    login state  + add errore page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 function ErrorPage() {
-  return (
-    <main>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-    </main>
+ return (
+    <div style={styles.container}>
+      <h1 style={styles.header}>404</h1>
+      <p style={styles.text}>Схоже, ми не можемо знайти потрібну вам сторінку</p>
+      <Link to="/" style={styles.button}>Перейти на головну</Link>
+    </div>
   );
 }
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '50px',
+  },
+  header: {
+    fontSize: '72px',
+  },
+  text: {
+    fontSize: '24px',
+  },
+  button: {
+    display: 'inline-block',
+    marginTop: '20px',
+    padding: '10px 20px',
+    backgroundColor: '#007bff',
+    color: '#fff',
+    textDecoration: 'none',
+    borderRadius: '4px',
+  }
+};
 
 export default ErrorPage;
