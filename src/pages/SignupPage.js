8323c3be8d07f2b3816a4c2177339d6345b7be10
Gilford0306 <Gilford0306@gmail.com>
2024-08-03 14:46:55 +0300

    add login input

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -4,51 +4,6 @@ import { Link } from 'react-router-dom';
 import Button from '../components/Button/Button'
 
 
-// const SignupPage = () => {
-//   return (
-//     <div className="registration-container">
-//       <div className="registration-header">
-//         <h1>Тепер давай зареєструємо тебе </h1>
-//       </div>
-//       <form className="registration-form">
-//         <div className="form-group">
-//           <input type="email" placeholder="Email*" />
-//         </div>
-//         <div className="form-group form-group-inline">
-//           <input type="text" placeholder="Ім'я*" />
-//           <input type="text" placeholder="Призвище*" />
-//         </div>
-//         <div className="form-group">
-//           <input type="password" placeholder="Пароль*" />
-//           <p className="password-hint">Мінімум символів: 8</p>
-//         </div>
-//         <div className="form-group">
-//           <input type="date" placeholder="Дата народження*" />
-//         </div>
-//         <div className="form-group form-group-checkbox">
-//           <label>
-//             <input type="checkbox" />
-//              Підписатися на розсилку...
-//            </label>
-//         </div>
-//         <div className="form-group form-group-checkbox">
-//           <label>
-//             <input type="checkbox" />
-//             Я приймаю <a><Link to="/privacy-policy">Політику конфіденційності</Link></a> та  <a><Link to={"/terms-of-uses"}> Умови використання</Link></a>.
-//           </label>
-//         </div>
-//         <div className="form-group">
-//           {/* <button type="submit" className="submit-button">Створити акаунт</button> */}
-//         <div className="form-group" style={{ alignSelf: 'flex-end' , marginTop: '20px'}}>
-//         <Button color="yellow" className="submit-button" text="Створити акаунт" href="/sport-store" />
-//         </div>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-
 function SignupPage() {
   return (
     <div className="registration-page">
@@ -59,6 +14,10 @@ function SignupPage() {
         <div className="form-group">
           <label htmlFor="email">Email*</label>
           <input type="text" id="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="login">Login*</label>
+          <input type="text" id="login" name="login" required />
         </div>
         <div className="form-group">
           <label htmlFor="firstName">Ім'я*</label>
